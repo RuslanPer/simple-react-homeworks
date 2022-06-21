106182719ed578cf650575b26e326168857f28b2
Ruslan <ruslan.pershin98@gmail.com>
2022-06-21 23:15:22 +0300

    Updates

---
+++ precache-manifest.f56e18cda0c3ded204cdc2f4d0fcb52e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23382a00f2a066953b2e4a76d79fae17",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "be4285cbc4121a5c4ed7",
+    "url": "/simple-react-homeworks/static/css/main.1555a2e2.chunk.css"
+  },
+  {
+    "revision": "66181dcc9bae39ea23ec",
+    "url": "/simple-react-homeworks/static/js/2.a0cd2b0e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-react-homeworks/static/js/2.a0cd2b0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4285cbc4121a5c4ed7",
+    "url": "/simple-react-homeworks/static/js/main.fd75df0e.chunk.js"
+  },
+  {
+    "revision": "35a9557543d5cb14c0c5",
+    "url": "/simple-react-homeworks/static/js/runtime-main.5a451111.js"
+  }
+]);
