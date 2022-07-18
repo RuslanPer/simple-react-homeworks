a6dc3029dd51b00c058ff662306a38f75b382448
Ruslan <ruslan.pershin98@gmail.com>
2022-07-18 23:01:39 +0300

    hw 9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,49 +1,64 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import style from './HW9.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date | null>()
     const [show, setShow] = useState<boolean>(false)
 
+
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+
+    const stringTime = date ? date.toLocaleString('ru', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    }) : ''
+
+
+    const stringDate = date ? date.toLocaleString('ru', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    }) : ''
 
     return (
         <div>
             <div
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
+                className={style.time}
             >
                 {stringTime}
             </div>
 
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
+            <div className={style.date}>
+                {show && stringDate}
+            </div>
 
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
+            <div className={style.buttons}>
+                <SuperButton onClick={start}>start</SuperButton>
+                <SuperButton onClick={stop}>stop</SuperButton>
+            </div>
+
 
         </div>
     )
