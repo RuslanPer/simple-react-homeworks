53f33fabd3cb910e0d2ed5f3a2517dc4aa3d675f
Ruslan <ruslan.pershin98@gmail.com>
2022-07-29 23:20:54 +0300

    hw10 redux

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,32 @@
 const initState = {
-
+	isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        case '': {
-            return state
-        }
-        default: return state
-    }
+type initStateType = typeof initState
+
+type LoadingActionType = {
+	type: 'LOADING'
+	isLoading: boolean
 }
 
-export const loadingAC = (): any => {} // fix any
+type ActionType = ReturnType<typeof loadingAC>
+
+export const loadingReducer = (
+	state: initStateType = initState,
+	action: ActionType
+): initStateType => {
+	switch (action.type) {
+		case 'LOADING': {
+			return { ...state, isLoading: action.isLoading }
+		}
+		default:
+			return state
+	}
+}
+
+export const loadingAC = (isLoading: boolean): LoadingActionType => {
+	return {
+		type: 'LOADING',
+		isLoading,
+	}
+}
--- src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Junior.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import HW7 from "../../h7/HW7";
-import HW8 from "../../h8/HW8";
-import HW9 from "../../h9/HW9";
+import HW10 from '../../h10/HW10'
+import HW7 from '../../h7/HW7'
+import HW8 from '../../h8/HW8'
+import HW9 from '../../h9/HW9'
 
 function Junior() {
-    return (
-        <div>
-            <HW7/>
-            <HW8/>
-            <HW9/>
-        </div>
-    )
+	return (
+		<div>
+			<HW7 />
+			<HW8 />
+			<HW9 />
+			<HW10 />
+		</div>
+	)
 }
 
 export default Junior
